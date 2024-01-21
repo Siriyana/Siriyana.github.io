ab1e53fc94a34dcc4b4c28b6160f1bd0eab0023e
Veera Kettunen <veera.kettunen@student.laurea.fi>
2024-01-21 20:20:10 +0200

    Lisää harjoituksia

---
+++ Javascript spring24/basic_harjoitus.js
@@ -0,0 +1,23 @@
+console.log('Hello world') ;
+
+const life = 100;
+const name = "Developer";
+
+var ika = 30 ;
+var nimi = "kissa" ;
+
+ika = ika + 1
+
+console.log(ika)
+console.log(nimi)
+
+var checkout = true;
+
+var box = null;
+
+function logger() {
+    console.log("Party time");
+    console.log("Party time");    
+}
+
+logger();
