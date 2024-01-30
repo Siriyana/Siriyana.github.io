9bded9234caf71094cd1552f79c35003f54dd980
Veera Kettunen <veera.kettunen@student.laurea.fi>
2024-01-30 15:49:07 +0200

    Lecture 3 examples
    Lecture on Events in Javascript

---
+++ Javascript spring24/JSlecture3.js
@@ -0,0 +1,20 @@
+function myFirstFunction(number){
+    for(var i=0; i < number; i++){
+        console.log(i);
+    }
+    return 10;
+}
+
+function buttonFunction(){
+    console.log("This event listener function");
+    var element = document.getElementById("demo");
+    element.innerHTML = "This is event listener text";
+}
+
+function showXY(e) {
+    var x = e.clientX;
+    var y = e.clientY;
+
+    var coords = "X coords: " + x + ", <br> Y coords: " + y;
+    document.getElementById("coordinates").innerHTML=coords;
+}
