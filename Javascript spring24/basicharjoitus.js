fba51df99fed4fe35b98064502bf0af63d4b4ba6
Veera Kettunen <veera.kettunen@student.laurea.fi>
2024-02-16 18:57:06 +0200

    SmoothScroll -harjoitus

---
+++ Javascript spring24/basicharjoitus.js
@@ -0,0 +1,38 @@
+
+function smoothScroll(target,duration) {
+    var target = document.querySelector(target);
+    var targetPosition = target.getBoundingClientRect().top; // Fix typo here
+    var startingPosition = window.pageYOffset;
+    var distance = targetPosition - startingPosition;
+    var startTime = null;
+
+    function animation(currentTime) {
+        if (startTime === null) startTime = currentTime;
+        var timeElapsed = currentTime - startTime;
+        var run = ease(timeElapsed, startingPosition, distance, duration);
+        window.scrollTo(0, run);
+        if (timeElapsed < duration) requestAnimationFrame(animation);
+    }
+
+    function ease(t, b, c, d) {
+        t /= d / 2;
+        if (t < 1) return c / 2 * t * t + b;
+        t--;
+        return -c / 2 * (t * (t - 2) - 1) + b;
+    }
+
+    requestAnimationFrame(animation);
+}
+
+var section1 = document.querySelector('.section1');
+var section2 = document.querySelector('.section2');
+
+section1.addEventListener('click', function () {
+    smoothScroll('.section2', 1000);
+});
+
+section2.addEventListener('click', function () {
+    smoothScroll('.section1', 1000);
+});
+
+smoothScroll('.section1', 1000);
