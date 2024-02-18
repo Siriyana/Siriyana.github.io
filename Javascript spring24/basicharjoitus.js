963c41c14e2ff39d57374a99dcddf5af7c2ae1f9
Veera Kettunen <veera.kettunen@student.laurea.fi>
2024-02-18 21:32:46 +0200

    WS5 exercise1
    Ainakin osittain tehty.

--- Javascript spring24/basicharjoitus.js
+++ Javascript spring24/basicharjoitus.js
@@ -36,3 +36,57 @@ section2.addEventListener('click', function () {
 });
 
 smoothScroll('.section1', 1000);
+
+function scrollAppear(){
+    var introText = document.querySelector('.intro-text');
+    var introPosition = introText.getBoundingClientRect().top;
+    console.log(introPosition);
+    var screenPosition = window.innerHeight/2; /* kun jakaa 2 teksti ilmestyy puolessa välissä, jos ilman /2, niin ilmestyy heti kun scrollaa..*/
+    if(introPosition < screenPosition){
+        introText.classList.add('intro-appear');
+    }
+}
+
+    window.addEventListener('scroll', scrollAppear);
+
+const carouselSlide = document.querySelector('.carousel-slide');
+const carouselImages = document.querySelectorAll('.carousel-slide img');
+
+//Buttons
+const prevButton = document.querySelector('#prevButton');
+const nextButton = document.querySelector('#nextButton');
+
+//Counter
+let counter = 1;
+const size = carouselImages[0].clientWidth; //gives our original width 
+
+carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+
+//Button listeners
+
+nextButton.addEventListener('click',()=>{
+    if(counter >= carouselImages.length -1) return;
+    carouselSlide.style.transition = "transform 0.4s ease-in-out";
+    counter++;
+    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+});
+
+prevButton.addEventListener('click',()=>{
+    if(counter <= 0) return;
+    carouselSlide.style.transition = "transform 0.4s ease-in-out";
+    counter--;
+    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+});
+
+carouselSlide.addEventListener('transitionend', () => {
+    if(carouselImages[counter].id === 'lastClone'){
+        carouselSlide.style.transition = "none";
+        counter = carouselImages.length -2;
+        carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+    }
+    if(carouselImages[counter].id === 'firstClone'){
+        carouselSlide.style.transition = "none";
+        counter = carouselImages.length -counter;
+        carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+    }
+});
--- Javascript spring24/js/shopping.js
+++ Javascript spring24/js/shopping.js
@@ -1,1 +1,24 @@
-Homework!
+function checkFields(){
+    var email = document.forms[0].email.value;
+    var comment = document.forms[0].comment.value;
+    
+    if(email==null || email=="" || email.length < 6 || email.length > 15 || email.indexOf("@") === -1){
+        document.getElementById("email").style.borderColor = "red";
+        document.getElementById("email").innerHTML = "<b>*Fill in properly</b>"
+        alert('Pieleen meni. Sähköpostissa pitää olla 7-14 merkkiä ja @-merkki');
+        return false;
+    } else {
+        alert('Email address: ' + email);
+    }
+
+    if(comment==null || comment=="" || comment.length > 50){
+        document.getElementById("comment").style.borderColor = "red";
+        document.getElementById("comment").innerHTML = "<b>*Fill in properly</b>"
+        alert('Pieleen meni... Kommenttikenttä on pakollinen. Maksimi 50 merkkiä.')
+        return false;
+    } else {
+        alert('Comments: ' + comment)
+    }
+
+    return true
+}
