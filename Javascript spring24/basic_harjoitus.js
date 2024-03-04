ddbd56b11875fce6a4f5aaa8853c2feca2fdddc5
Veera Kettunen <veera.kettunen@student.laurea.fi>
2024-03-04 20:23:24 +0200

    harjoituksia

--- Javascript spring24/basic_harjoitus.js
+++ Javascript spring24/basic_harjoitus.js
@@ -118,3 +118,28 @@ addListBtn.addEventListener("click", function() {
     // Attaching the li to the user list
     userList.appendChild(newLi);
 });
+
+
+var table = document.getElementById("table"),rIndex;
+
+for(var i = 1; i < table.rows.length; i++){
+    table.rows[i].onclick = function(){
+        rIndex = this.rowIndex;
+
+        document.getElementById("fname").value = this.cells[1].innerHTML;
+        document.getElementById("lname").value = this.cells[2].innerHTML;
+        document.getElementById("country").value = this.cells[3].innerHTML;
+        document.getElementById("mNumber").value = this.cells[4].innerHTML;
+    };
+}
+
+function editRow(){
+    table.rows[rIndex].cells[1].innerHTML = document.getElementById("fname").value;
+    table.rows[rIndex].cells[2].innerHTML = document.getElementById("lname").value;
+    table.rows[rIndex].cells[3].innerHTML = document.getElementById("country").value;
+    table.rows[rIndex].cells[4].innerHTML = document.getElementById("mNumber").value;
+}
+
+function editTableDisplay(){
+    document.querySelector('.editTable').setAttribute('style', 'display: block;')
+}
