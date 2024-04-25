2c21ab2c35e96cee21d411c7df20246b7e909e52
Veera Kettunen <veera.kettunen@student.laurea.fi>
2024-04-25 19:02:39 +0300

    Projekti2 asemanhaku_onnistuu

--- Javascript spring24/peli3.js
+++ Javascript spring24/peli3.js
@@ -3,35 +3,85 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 CANVAS_WIDTH = canvas.width = 500;
 CANVAS_HEIGHT = canvas.height = 1000;
+const numberOfEnemies = 10;
+const enemiesArray = [];
 
-/*
-enemy1 = {
-    x: 0,
-    y: 0,
-    width: 200,
-    height: 200,
-}*/
 
-class Enemy {
+let gameFrame = 0;
+
+class Enemy1 {
     constructor(){
-        this.x = 10;
-        this.y = 50;
-        this.width = 100;
-        this.height = 100;
+        this.image = new Image();
+        this.image.src = 'enemies/enemy1.png';
+        //this.speed = Math.random() * 4 - 2; //random number between -2 to 2 (0-2 4-2)
+        this.spriteWidth = 293;
+        this.spriteHeight = 155;
+        this.width = this.spriteWidth / 2.5;       
+        this.height = this.spriteHeight / 2.5;
+        this.x = Math.random() * (canvas.width - this.width);  //randomize the starting point of enemy, but - this.width makes sure it is inside canvas
+        this.y = Math.random() * (canvas.height - this.height);
+        this.frame = 0;
+        this.flapSpeed = Math.floor(Math.random() * 3 +1);
+    }
+    //method to define where enemy spawns and at what speed and direnction it goes
+    update() {
+        this.x += Math.random() * 3 - 1.5;
+        this.y += Math.random() * 3 -1.5;
+        //animate sprites
+        if (gameFrame % this.flapSpeed === 0) {
+            this.frame > 4 ? this.frame = 0 : this.frame++;
+        }
+        
+    }
+    //draws enemy
+    draw(){
+        ctx.drawImage(this.image, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
     }
 };
 
-const enemy1 = new Enemy();
-const enemy2 = new Enemy();
+class Enemy2 {
+    constructor(){
+        this.image = new Image();
+        this.image.src = 'enemies/enemy2.png';
+        this.speed = Math.random() * 4 - 6; //random number between -2 to 2 (0-2 4-2)
+        this.spriteWidth = 266;
+        this.spriteHeight = 188;
+        this.width = this.spriteWidth / 2.5;       
+        this.height = this.spriteHeight / 2.5;
+        this.x = Math.random() * (canvas.width - this.width);  //randomize the starting point of enemy, but - this.width makes sure it is inside canvas
+        this.y = Math.random() * (canvas.height - this.height);
+        this.frame = 0;
+        this.flapSpeed = Math.floor(Math.random() * 3 +1);
+    }
+    //method to define where enemy spawns and at what speed and direnction it goes
+    update() {
+        this.x += this.speed;
+        //this.y += Math.random() * 3 -1.5;
+        if (this.x + this.width < 0) this.x = canvas.width;
+        //animate sprites
+        if (gameFrame % this.flapSpeed === 0) {
+            this.frame > 4 ? this.frame = 0 : this.frame++;
+        }
+        
+    }
+    //draws enemy
+    draw(){
+        ctx.drawImage(this.image, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
+    }
+};
+
+//create enemies and save them to array
+for (let i = 0; i < numberOfEnemies; i++){
+    enemiesArray.push(new Enemy2());     //created enemies are pushed to array
+}
 
 function animate() {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    enemy1.x++;
-    enemy1.y++;
-    enemy2.x+=0.5;
-    enemy2.y++;
-    ctx.fillRect(enemy1.x, enemy1.y, enemy1.width, enemy1.height);
-    ctx.fillRect(enemy2.x, enemy2.y, enemy2.width, enemy2.height);
+    enemiesArray.forEach(enemy => {
+        enemy.update();
+        enemy.draw();
+    });
+    gameFrame++;
     requestAnimationFrame(animate);
 }
 animate();
